66883fbbbf60bde72ac3eccd3b0023bd9f9d61c6
JZwlth <wkjazy87@gmail.com>
2024-10-27 21:31:40 -0700

    update

--- main.cpp
+++ main.cpp
@@ -1,16 +1,24 @@
 #include <iostream>
-#include <chrono>
-using namespace std;
+#include <fstream>
+#include <string>
 
-int main() {
-    
+using namespace std;
+using namespace std::chrono;
+
+
+
+    for (const auto& op : operations) {
+        cout << setw(12) << op;
+        for (const auto& ds : data_structures) {
+            int time = times[ds][op];
+            if (time == -1) {
+                cout << setw(12) << "-1";
+            } else {
+                cout << setw(12) << time;
+            }
+        }
+        cout << endl;
+    }
 
     return 0;
 }
-
-/* syntax examples:
-auto start = high_resolution_clock::now()
-auto end = high_resolution_clock::now()
-auto duration = duration_cast<milliseconds>(end - start)
-duration.count() references elapsed milliseconds
-*/
